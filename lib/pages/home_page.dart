a5c95db03713bbf1e9396d9f1f9ce61b4a5bc297
Dipen Napit <dipennapit45@gmail.com>
2025-04-21 05:48:11 +0545

    Local file,load and decode json

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:my_app/widgets/catalog.dart';
 import 'package:my_app/widgets/drawer.dart';
 import 'package:my_app/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+
+    final decodeData =  jsonDecode(catalogJson);
+    var productsData=decodeData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
